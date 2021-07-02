e039aa897951b9ea81f922ae9e3bcc33d7e96ef1
nischal shrestha <nischalstha9@gmail.com>
2021-07-02 11:49:09 +0545

    include nav with php

---
+++ nav.php
@@ -0,0 +1,25 @@
+<nav>
+    <div class="logo">Sharing is Caring</div>
+    <div class="nav-links">
+        <div class="nav-item">
+            <a href="#">
+                About Us
+            </a>
+        </div>
+        <div class="nav-item">
+            <a href="#">
+                Contact Us
+            </a>
+        </div>
+        <div class="nav-item">
+            <a href="#">
+                Sign In
+            </a>
+        </div>
+        <button class="btn btn-login">
+            <a href="#">
+                Register
+            </a>
+        </button>
+    </div>
+</nav>
