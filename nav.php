8eaa33a09439e2071b2ae95934199207d893510e
nischal shrestha <nischalstha9@gmail.com>
2021-07-02 12:42:12 +0545

    better scrollbar

--- nav.php
+++ nav.php
@@ -2,22 +2,22 @@
     <div class="logo">Sharing is Caring</div>
     <div class="nav-links">
         <div class="nav-item">
-            <a href="#">
+            <a href="#" class='nav-link'>
                 About Us
             </a>
         </div>
         <div class="nav-item">
-            <a href="#">
+            <a href="#" class='nav-link'>
                 Contact Us
             </a>
         </div>
         <div class="nav-item">
-            <a href="#">
+            <a href="#" class='nav-link'>
                 Sign In
             </a>
         </div>
         <div class="nav-item">
-            <a href="#">
+            <a href="#" class='nav-link'>
                 <img src="./public/dog.jpeg" alt="" srcset="" class='avatar'>
             </a>
         </div>
