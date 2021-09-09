d2e0117e2c3a886ae8c15333713915a8b1fd52cf
Nischal Shrestha <nischalstha9@gmail.com>
2021-09-09 09:15:14 +0545

    touchup

--- sharing_is_caring.sql
+++ sharing_is_caring.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.5deb2
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
--- Host: localhost:3306
--- Generation Time: Sep 04, 2021 at 01:29 PM
--- Server version: 8.0.26-0ubuntu0.20.04.2
--- PHP Version: 7.4.3
+-- Host: 127.0.0.1
+-- Generation Time: Sep 09, 2021 at 05:29 AM
+-- Server version: 10.4.13-MariaDB
+-- PHP Version: 7.4.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -16,7 +15,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `sharing_is_caring`
@@ -29,16 +28,16 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `helps` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
   `description` text NOT NULL,
   `location` varchar(255) NOT NULL,
-  `contact` int NOT NULL,
-  `helper_id` int UNSIGNED NOT NULL,
-  `category` int UNSIGNED DEFAULT NULL,
-  `created_at` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `active` int NOT NULL DEFAULT '1'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+  `contact` int(11) NOT NULL,
+  `helper_id` int(10) UNSIGNED NOT NULL,
+  `category` int(10) UNSIGNED DEFAULT NULL,
+  `created_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `active` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `helps`
@@ -47,7 +46,9 @@ CREATE TABLE `helps` (
 INSERT INTO `helps` (`id`, `title`, `description`, `location`, `contact`, `helper_id`, `category`, `created_at`, `active`) VALUES
 (2, 'First Help', 'Ge', 'ktm', 98, 5, 1, '2021-09-04 12:38:21', 1),
 (3, 'First Help', 'momo', 'mo', 98, 5, 2, '2021-09-04 12:38:21', 1),
-(4, 'Timestan', 'tim3e', 'bkt', 89, 5, 1, '2021-09-04 12:38:45', 1);
+(4, 'Timestan', 'tim3e', 'bkt', 89, 5, 1, '2021-09-04 12:38:45', 1),
+(5, 'Windows Help', 'Mero other help', 'nkt', 787898, 5, 2, '2021-09-05 20:19:22', 1),
+(6, 'Jacket', '', 'Kathmandu', 2147483647, 1, 2, '2021-09-06 08:57:49', 1);
 
 -- --------------------------------------------------------
 
@@ -56,10 +57,10 @@ INSERT INTO `helps` (`id`, `title`, `description`, `location`, `contact`, `helpe
 --
 
 CREATE TABLE `helps_category` (
-  `id` int NOT NULL,
+  `id` int(11) NOT NULL,
   `name` varchar(256) NOT NULL,
   `description` varchar(1000) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `helps_category`
@@ -76,10 +77,10 @@ INSERT INTO `helps_category` (`id`, `name`, `description`) VALUES
 --
 
 CREATE TABLE `session_token` (
-  `id` int NOT NULL,
-  `user_id` int UNSIGNED NOT NULL,
+  `id` int(11) NOT NULL,
+  `user_id` int(10) UNSIGNED NOT NULL,
   `token` varchar(100) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -88,21 +89,21 @@ CREATE TABLE `session_token` (
 --
 
 CREATE TABLE `user` (
-  `id` int UNSIGNED NOT NULL COMMENT 'User Id',
+  `id` int(10) UNSIGNED NOT NULL COMMENT 'User Id',
   `username` varchar(50) NOT NULL COMMENT 'username',
   `email` varchar(200) NOT NULL COMMENT 'email',
   `password` varchar(1000) NOT NULL COMMENT 'password',
-  `active` int UNSIGNED NOT NULL DEFAULT '1',
-  `email_verified` int NOT NULL DEFAULT '0',
-  `role` int NOT NULL DEFAULT '1'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+  `active` int(10) UNSIGNED NOT NULL DEFAULT 1,
+  `email_verified` int(11) NOT NULL DEFAULT 0,
+  `role` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `username`, `email`, `password`, `active`, `email_verified`, `role`) VALUES
-(1, '', 'nischal@g.com', '$2y$10$5OO2NXCAsLej7wmvZacaXePxG0dn5m2XCK49vi1zk6W3wsSKLn9Gy', 1, 0, 0);
+(1, '', 'nischal@g.com', '$2y$10$pV3.NX4l2GeWxwWcVGv9vemtdgCaFCLLvCFGcKatzXNUDRaygaV8q', 1, 0, 0);
 
 --
 -- Indexes for dumped tables
@@ -141,25 +142,25 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `helps`
 --
 ALTER TABLE `helps`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `helps_category`
 --
 ALTER TABLE `helps_category`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `session_token`
 --
 ALTER TABLE `session_token`
-  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'User Id', AUTO_INCREMENT=7;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'User Id', AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
